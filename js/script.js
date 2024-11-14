5b44b39d827813c9eec93eee9f6d5fb514dafbee
BartCatalano <catalanobartolomeo@gmail.com>
2024-11-14 10:31:52 +0100

    feat: sistemata card base

--- js/script.js
+++ js/script.js
@@ -44,17 +44,21 @@ const cardEmpty = document.getElementById("card-empty");
  function cardCreator(member) {
   const {name, role, email, img} = member;
    return `
-  <img src="${img}" class="img-fluid rounded-start" alt="...">
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">${name}</h5>
-                  <p class="card-text">${role}</p>
-                  <p class="card-text">${email}</p>
-                  </div>
-                  </div>
-                  
-   `;
+   <div id="card-empty" class="card mb-3" style="max-width: 540px;">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="${img}" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <p class="card-text">${role}</p>
+            <p class="card-text">${email}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+   `
 
  };
 
